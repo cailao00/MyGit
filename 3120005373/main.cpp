2fd1d51f5b4c02a64f627e305ab24f30e8bd20b4
cailao <1364625070@qq.com>
2022-09-16 22:57:25 +0800

    first commit

---
+++ 3120005373/main.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#define MAXSIZE 10000
+using namespace std;
+/*
+论文查重
+*/
+
+typedef struct sentence {
+	string word;	//记录句子
+	int len;		//记录字数
+}sentence;
+
+sentence sen[MAXSIZE];	//存放读入的原文
+sentence csen[MAXSIZE];	//存放读入的抄文
+int num[5000][5000];	//记录状态值，用于识别字符是否相同
+int olen = 0;			//原文本长度
+int clen = 0;			//抄文本长度
+//int sum = 0;
+double Num = 0;			//查重数
+void LCS(int len);
+
+void original(string file) {	//原文本输入
+	ifstream ifs;
+	ifs.open(file, ios::in);	//以读的方法打开文件
+	if (!ifs.is_open()) {
+		cout << "文件读取失败" << endl;
+		return;
+	}
+	else {
+		string s;
+		while (getline(ifs,s)) {
+			sen[olen].word = s;
+			sen[olen].len = s.length();
+			olen++;
+		}
+	}
+	ifs.close();
+}
+
+void copy(string file) {		//抄袭文本输入
+	ifstream ifs;
+	ifs.open(file, ios::in);
+	if (!ifs.is_open()) {
+		cout << "文件打开失败！" << endl;
+		return;
+	}
+	else {
+		string s;
+		while (getline(ifs, s)) {
+			csen[clen].word = s;
+			csen[clen].len = s.length();
+			LCS(clen);
+			clen++;
+		}
+		cout << "文件打开成功！" << endl;
+	}
+	ifs.close();
+}
+
+void LCS(int len) {			//用动态规划算法求解
+	int max = 0;
+	string a, b;
+	for (int i = 0; i < sen[len].len; i++) {
+		for (int j = 0; j < csen[len].len; j++) {
+			a = sen[len].word;
+			b = csen[len].word;
+			if (a[i] == b[j]) {
+				num[i + 1][j + 1] = num[i][j] + 1;
+				max = num[i + 1][j + 1] > max ? num[i + 1][j + 1] : max;
+				if (max >= 7) {
+					Num++;
+					break;
+				}
+			}
+			else if (num[i][j + 1] > num[i + 1][j]) {
+				num[i + 1][j + 1] = num[i][j + 1];
+			}
+			else {
+				num[i + 1][j + 1] = num[i + 1][j];
+			}
+		}
+		if (max >= 7) {
+			break;
+		}
+	}
+}
+
+int main() {
+	original("E://Visual Studio//text//orig.txt");
+	copy("E://Visual Studio//text//orig_0.8_dis_10.txt");
+	memset(num, 0, olen);
+	//double rate = Num / sum;
+	cout << "copy rate:" << 2 * Num / (olen - 1) << endl;	//空行不算
+	cout << Num << " " << olen << endl;
+
+	system("pause");
+	return 0;
+}
